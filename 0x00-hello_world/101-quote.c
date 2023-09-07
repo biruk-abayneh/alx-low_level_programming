8abd648172aa99218c456e51607b7f11b2462997
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-07 12:24:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the function
+ * Description: This function prints a test to the error 
+ * Return : 1 success
+ */
+int main(void)
+{
+fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
