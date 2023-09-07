8b8d74349407dde76d4ecd45d60fef0584b711d9
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-07 13:08:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - The entry point of the function
- * Description: This function prints a test to the error 
- * Return : 1 success
+ * Description: This function prints a test to the error
+ * Return: 1 success
  */
 int main(void)
 {
-fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+printf("\n");
 return (1);
 }
