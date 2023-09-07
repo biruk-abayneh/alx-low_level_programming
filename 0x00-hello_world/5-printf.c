0a925adb72991d1a5d7c16dca48eac36a1926954
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-07 11:35:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This function prints a string
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
