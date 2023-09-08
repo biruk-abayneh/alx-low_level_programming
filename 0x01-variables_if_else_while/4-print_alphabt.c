5d9bf889d7707a4ad89c7b589a9b4bac273418b5
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-08 13:14:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main is entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+char letter[26] = "abcdfghijklmnoprstuvwxyz";
+int i = 0;
+
+while (i < 26)
+{
+putchar(letter[i]);
+i++;
+}
+printf("\n");
+return (0);
+}
