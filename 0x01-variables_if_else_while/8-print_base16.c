28b6e61b92a4997ad2d7c9e223b0bd900f79b32b
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-08 13:36:06 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,14 @@
 
 int main(void)
 {
-int letter[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char letter[16] = "0123456789abcdef";
 int i = 0;
 
-while (i < 10)
+while (i < 16)
 {
 putchar(letter[i]);
 i++;
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
