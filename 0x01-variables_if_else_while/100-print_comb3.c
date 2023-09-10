c7beee89b6900bbe0bae24c760b9f3ca3322cddb
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-10 14:11:28 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main is entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar((i != 8 || j != 9) ? ',' : '\n');
+}
+}
+return (0);
+}
