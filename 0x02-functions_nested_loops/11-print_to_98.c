18787878d5218f628af1cec0d91c03000f0cbdef
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 14:32:37 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers
+ * @n: integer
+ * Return: result
+ */
+
+void print_to_98(int n)
+{
+for (i = n; i < 99; i++)
+{
+_putchar('0' + i);
+_putchar((i == 98) ? ',' : '\0');
+_putchar((i == 98) ? ' ' : '\0');
+return (x);
+}
