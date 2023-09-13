d65566069bb73fa2097a8ed99c16a3838105dc26
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 11:54:56 -0700

    0-putchar.c 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * main - print _putchar
  * Return: 0 and exit
@@ -7,14 +6,14 @@
 
 int main(void)
 {
-_putchar(_);
-_putchar(p);
-_putchar(u);
-_putchar(t);
-_putchar(c);
-_putchar(h);
-_putchar(a);
-_putchar(r);
-_putchar(\n);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
