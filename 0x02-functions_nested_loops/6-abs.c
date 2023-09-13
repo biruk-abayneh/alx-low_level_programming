80efb5ac32ea252506fd6f74739c9c8e2e6209a3
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 13:11:14 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - pring the abssolute value of characher
+ * @n: number to be checked
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else if (n >= 0)
+{
+return (n);
+}
+}
