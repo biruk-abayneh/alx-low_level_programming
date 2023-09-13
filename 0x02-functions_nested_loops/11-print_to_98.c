1e366a88b9eb53b7e63e141eaf04340c38b5d87f
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 14:52:49 -0700

    11-print_to_98.c 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,27 @@ void print_to_98(int n)
 {
 int i;
 
+if (n < 98)
+{
 for (i = n; i < 99; i++)
 {
-_putchar('0' + i);
+_putchar(i);
 _putchar((i == 98) ? ',' : '\0');
-_putchar((i == 98) ? ' ' : '\0');
+_putchar((i == 98) ? ' ' : '\n');
+}
+}
+else if (n > 98)
+{
+for (i = n; i > 97; i--)
+{
+_putchar(i);
+_putchar((i == 98) ? ',' : '\0');
+_putchar((i == 98) ? ' ' : '\n');
+}
+}
+else if (n == 98)
+{
+_putchar('0' + n);
+_putchar('\n');
 }
 }
