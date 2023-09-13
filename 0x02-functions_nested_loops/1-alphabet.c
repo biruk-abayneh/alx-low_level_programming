6cf6c5b920a76261a02830256e766bbc59870d05
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 12:14:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_alphabet - print alphabet
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+/**
+ * print_alphabet - print alphabet
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+		  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int i;
+
+for (i = 0; i < 26; i++)
+{
+_putchar(lower[i]);
+}
+_putchar('\n');
+}
