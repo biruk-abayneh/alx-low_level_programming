a6a22e767b2eda9ead39513cbab7fba5f7d26c84
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 15:03:27 -0700

    11-print_to_98.c 5

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,23 +13,24 @@ if (n < 98)
 {
 for (i = n; i < 99; i++)
 {
-_putchar(i);
-_putchar((i == 98) ? ',' : '\0');
-_putchar((i == 98) ? ' ' : '\n');
+if (i != 98)
+printf("%d", i);
+else if (i == 98)
+printf("%d\n", i);
 }
 }
 else if (n > 98)
 {
 for (i = n; i > 97; i--)
 {
-_putchar(i);
-_putchar((i == 98) ? ',' : '\0');
-_putchar((i == 98) ? ' ' : '\n');
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
 }
 }
 else if (n == 98)
 {
-_putchar('0' + n);
-_putchar('\n');
+printf("98\n");
 }
 }
