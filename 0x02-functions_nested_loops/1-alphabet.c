dfa5526fbd079e4efb30065649dc3726fa65443a
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-13 12:17:48 -0700

    1-alphabet.c 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- * print_alphabet - print alphabet
- * Return: Always 0.
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-/**
  * print_alphabet - print alphabet
  * Return: Always 0.
  */
