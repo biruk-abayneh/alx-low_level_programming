4e7cda73aec06a764d874eda9c4d028905cb4245
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-14 10:03:10 -0700

    100-times_table.c 6

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,19 @@ for (i = 0; i < n + 1; i++)
 for (j = 0; j < n + 1; j++)
 {
 k = i * j;
+if (k < 10)
 _putchar('0' + k);
+else if ((k > 10) && (k < 100))
+{
+_putchar('0' + (k / 10));
+_putchar('0' + (k % 10));
+}
+else
+{
+_putchar('0' + (k / 100));
+_putchar('0' + ((k / 10) % 10));
+_putchar('0' + (k % 10));
+}
 if (j == n)
 {
 _putchar('\n');
