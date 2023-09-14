60e576c677aea86ba0870ae149c9b07f53696cd8
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-14 10:12:30 -0700

    100-times_table.c 7

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,18 +20,8 @@ for (j = 0; j < n + 1; j++)
 {
 k = i * j;
 if (k < 10)
+{
 _putchar('0' + k);
-else if ((k >= 10) && (k < 100))
-{
-_putchar('0' + (k / 10));
-_putchar('0' + (k % 10));
-}
-else
-{
-_putchar('0' + (k / 100));
-_putchar('0' + ((k / 10) % 10));
-_putchar('0' + (k % 10));
-}
 if (j == n)
 {
 _putchar('\n');
@@ -42,6 +32,36 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
+}
+}
+else if ((k >= 10) && (k < 100))
+{
+_putchar('0' + (k / 10));
+_putchar('0' + (k % 10));
+ if (j == n)
+{
+_putchar('\n');
+}
+else if (j < n)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar('0' + (k / 100));
+_putchar('0' + ((k / 10) % 10));
+_putchar('0' + (k % 10));
+if (j == n)
+{
+_putchar('\n');
+}
+else if (j < n)
+{
+_putchar(',');
+_putchar(' ');
+}
 }
 }
 }
