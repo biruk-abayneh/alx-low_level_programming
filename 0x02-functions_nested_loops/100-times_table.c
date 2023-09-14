5fd7f8a403e117d6e5838720cd8dae0c993318f3
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-14 09:38:54 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_times_table - print n times table
+ * @n: number of table
+ * Return: table
+ */
+
+void print_times_table(int n)
+{
+int i, j, k;
+
+if ((n < 0) || (n > 15))
+_putchar('\0')
+else
+{
+for (i = 0; i < n + 1; i++)
+{
+for (j = 0; j < n + 1; j++)
+{
+k = i * j;
+_putchar(k);
+if (j == n)
+{
+_putchar('\n');
+}
+else if (j < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+}
+}
