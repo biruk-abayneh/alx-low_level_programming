f3bd18dd970313845559eb1444fcd16a7300dacf
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-14 09:43:38 -0700

    100-times_table.c 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,12 +7,13 @@
 
 void print_times_table(int n)
 {
-int i, j, k;
 
 if ((n < 0) || (n > 15))
-_putchar('\0')
+_putchar('\0');
 else
 {
+int i, j, k;
+
 for (i = 0; i < n + 1; i++)
 {
 for (j = 0; j < n + 1; j++)
