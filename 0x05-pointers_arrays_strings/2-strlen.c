cae8cbb7f90fbf7c1b947175adc9c4e7faf779ea
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-18 09:44:52 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @s: string to check
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int count;
+
+while (*s != '\0')
+{
+count++;
+s++;
+}
+return (count);
+}
