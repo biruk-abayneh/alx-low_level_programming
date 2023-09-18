6b759c623824c16695ce2a06dd29dbcdc219cd9c
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-18 13:41:10 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * puts_half - print half of string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+int c = 0;
+int i;
+char *a = str;
+
+while (*a != '\0')
+{
+a++;
+c++;
+}
+if (c % 2 == 0)
+{
+for (i = (c / 2); i < c; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for (i = ((c - 1) / 2); i < c; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
