132ba4b86897b4f0cb0a7d74477453aac6e16967
biruk-abayneh <birukabayneh2@gmail.com>
2023-09-21 12:38:20 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize the string
+ * @n: string to check
+ * Return: checked string
+ */
+
+char *cap_string(char *n)
+{
+int i = 0;
+
+while (n[i])
+{
+if (n[i] == ' ' || n[i] == '\n' || n[i] == '\t')
+{
+if (n[(i + 1)] >= 'a' && n[(i + 1)] <= 'z')
+{
+n[(i + 1)] = n[(i + 1)] - 32;
+}
+}
+i++;
+}
+return (n);
+}
