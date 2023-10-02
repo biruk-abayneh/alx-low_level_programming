61de2d533878f6876d93f02b8139f0e30e2601f1
biruk-abayneh <birukabayneh2@gmail.com>
2023-10-02 06:56:50 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print number of CLA
+ * @argc: number of CLA
+ * @argv: CLA
+ * Return: 0 and success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
