b129d6fa68c479c5f4265edc33a8f7652aab98ce
Biruk <Birukabayneh2@gmail.com>
2023-10-06 23:43:07 +0300

    2-calloc.c 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
+char *ptr;
+unsigned int i;
 
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
 	return (NULL);
-if ((nmemb = 0) || (size == 0))
+if ((nmemb == 0) || (size == 0))
 	return (NULL);
+for (i = 0; i < (nmemb * size); i++)
+{
+    ptr[i] = '0';
+}
 return (ptr);
 }
