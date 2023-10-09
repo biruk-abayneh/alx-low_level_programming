519eb2020bb24f5df7c31113c9e141c18b514986
Biruk <Birukabayneh2@gmail.com>
2023-10-09 17:03:30 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * dog - alias for dog
+ * Description: information about dog
+ */
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+
