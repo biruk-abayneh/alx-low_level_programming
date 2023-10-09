954a9417b3510dbf82d36ec77593a49a845c9502
biruk-abayneh <birukabayneh2@gmail.com>
2023-10-09 11:18:39 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,7 @@ char *owner;
  */
 typedef struct dog dog_t;
 
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 char *_strcpy(char *dest, char *src);
