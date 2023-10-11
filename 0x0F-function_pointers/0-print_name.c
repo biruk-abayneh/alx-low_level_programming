4d6509df9de4082f1b2192f2aa2528bc5ac50f7e
Biruk <Birukabayneh2@gmail.com>
2023-10-11 18:34:26 +0300

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - calls other print functions
+ * @name: name to print
+ * @f: function to call
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+void (*ptr)(char *) = *f;
+
+(*ptr)(name);
+}
