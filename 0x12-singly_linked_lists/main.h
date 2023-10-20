56066586bf97860d5ba721ff77d903fd506e3907
Biruk <Birukabayneh2@gmail.com>
2023-10-20 22:09:04 +0300

    all

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+size_t list_len(const list_t *h);
+int _strlen(const char *s);
+list_t *add_node(list_t **head, const char *str);
+unsigned int _strlen(char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 size_t print_list(const list_t *h);
 
 #endif
