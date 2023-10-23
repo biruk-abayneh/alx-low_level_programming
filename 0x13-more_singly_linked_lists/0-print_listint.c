6332d0f74bd6afd1ad80e96c87e598d5579671c2
biruk-abayneh <birukabayneh2@gmail.com>
2023-10-23 11:20:56 -0700

    0-print_listint.c 2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,16 +3,22 @@
 #include <stdarg.h>
 #include "lists.h"
 
+/**
+ * print_listint - print the contents of list
+ * @h: list
+ * Return: number of print
+ */
+
 size_t print_listint(const listint_t *h)
 {
-    unsigned int a;
+unsigned int a;
 
-    a = 0;
-    while (h != NULL)
-    {
+a = 0;
+while (h != NULL)
+{
 	printf("%d\n", h->n);
 	a++;
 	h = h->next;
-    }
-    return (a);
+}
+return (a);
 }
