3af47dc722c5ad0af739da29168156908b0209bd
Biruk <Birukabayneh2@gmail.com>
2023-11-15 21:08:12 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,35 +4,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
     FILE *a;
     char *buf;
-    size_t *bytesread;
-    ssize_t *byteswritten;
+    size_t bytesread;
+    ssize_t byteswritten;
     
     if (filename == NULL)
 	return (0);
     
-    a = fopen(filename, O_RDWR);
-    if (a == "-1")
+    a = fopen(filename, "r");
+    if (a == NULL)
     {
-	fclose(fd);
+	fclose(a);
 	return (0);
     }
 
     buf = malloc((letters + 1) * sizeof(char));
-    bytesread = fopen(buf, sizeof(char), letters, fd);
+    bytesread = fread(buf, sizeof(char), letters, a);
     if (bytesread == 0)
     {
+	fclose(a);
 	free(buf);
-	fclose(fd);
 	return(0);
     }
-    buf[bytesread] = "\0";
-    byteswritten = fwrite(buf, sizefo(char), bytesread, stdout);
-    if (byteswritten != bytesread)
-    {
-	free(buf);
-	fclose(fd);
-	return (0);
-    }
-    return (bytesread);
+    buf[bytesread] = '\0';
+    byteswritten = fwrite(buf, sizeof(char), bytesread, stdout);
+
+    return (byteswritten);
     
 }
